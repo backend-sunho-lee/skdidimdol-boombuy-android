720fdb076b8bd213b4c74e233013c6c6ab602d6e
jimin <jimin6407@naver.com>
2017-03-06 08:57:30 +0900

    fcm수정

--- app/src/main/java/com/taca/boombuy/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/taca/boombuy/fcm/MyFirebaseMessagingService.java
@@ -57,7 +57,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("FCM Message")
+                .setContentTitle("BOOMBUY")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)

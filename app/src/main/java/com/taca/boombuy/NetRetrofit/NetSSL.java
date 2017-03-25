01a45e05d76a6ea34c541ff6f050a6c221a5fe2a
Jimin Kim <jimin6407@naver.com>
2017-03-26 01:18:44 +0900

    서버주소 수정, 배너 사이즈 수정

--- app/src/main/java/com/taca/boombuy/NetRetrofit/NetSSL.java
+++ app/src/main/java/com/taca/boombuy/NetRetrofit/NetSSL.java
@@ -71,7 +71,7 @@ public class NetSSL {
             client = clientTmp.build();
             // ====================================================================================
             retrofit = new Retrofit.Builder()
-                    .baseUrl("https://ec2-52-78-52-228.ap-northeast-2.compute.amazonaws.com")
+                    .baseUrl("https://ec2-13-124-73-64.ap-northeast-2.compute.amazonaws.com")
                     .addConverterFactory(GsonConverterFactory.create())
                     .client(client)
                     .build();
